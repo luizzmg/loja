f1db76e8d81c089f3bea0015a9c937b531640eed
luizzmg <luizzmiguel17@gmail.com>
2023-10-22 22:04:45 -0300

    editando carrinho

--- src/chocorato/Carrinho.java
+++ src/chocorato/Carrinho.java
@@ -1,5 +1,20 @@
 package chocorato;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Carrinho {
+    static Map<String, Float> produtos = new HashMap<>();
+
     float totalCompra;
+
+    public static void main(String[] args) {
+        produtos.put("Trufas de chocolate ao leite", 2.23f);
+        produtos.put("Trufas de chocolate amargo", 4.64f);
+
+        System.out.print("Digite o produto desejado para saber o preço: ");
+        String consulta = Global.scan.nextLine();
+
+        System.out.println("O preço de "+ consulta +" é "+ produtos.get(consulta));
+
+    }
 }
