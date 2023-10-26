45c35572a0d575af6d87812a0151314e5f8ffdda
RodrigoAlSantos <148580913+RodrigoAlSantos@users.noreply.github.com>
2023-10-26 01:36:50 -0300

    Update Doce.java

--- src/chocorato/Doce.java
+++ src/chocorato/Doce.java
@@ -6,27 +6,27 @@ public class Doce{
    
     static String[] nomes = 
     {
-        "Gomas de mascar",
-        "Gomas de frutas em formatos variados",
-        "Marshmallows artesanais",
-        "Balas de caramelo",
-        "Pirulitos gourmet",
-        "Jujubas coloridas",
-        "Pastilhas de menta",
+        "Goma de mascar",
+        "Goma de fruta em formatos variados",
+        "Marshmallow artesanal",
+        "Bala de caramelo",
+        "Pirulito gourmet",
+        "Jujuba colorida",
+        "Pastilha de menta",
         "Drops de frutas",
-        "Doces de leite"
+        "Doce de leite"
     };
 
     static float[] precos = 
     {
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
+        0,25,
+        1,
+        1,50,
+        0,50,
+        2,98,
+        0,25,
+        0,25,
+        0,45,
+        8,98
     };
 }
