1633dc7d5fe03d6553e6dda6b1b75d4bc321902b
RodrigoAlSantos <148580913+RodrigoAlSantos@users.noreply.github.com>
2023-10-26 01:38:40 -0300

    Update Sobremesa.java

--- src/chocorato/Sobremesa.java
+++ src/chocorato/Sobremesa.java
@@ -6,25 +6,23 @@ public class Sobremesa {
    
     static String[] nomes = 
     {
-        "Brownies de chocolate",
+        "Brownie de chocolate",
         "Fondue de chocolate com frutas frescas",
-        "Bolos de chocolate decorados",
-        "Tortas de chocolate",
-        "Milkshakes de chocolate",
-        "Crepes com chocolate derretido",
-        "Sorvetes de baunilha e cobertura de chocolate"
+        "Bolo de chocolate misto (fatia)",
+        "Torta de chocolate gourmet (fatia)",
+        "Milkshake de chocolate",
+        "Crepe com chocolate derretido",
+        "Sorvete de baunilha e cobertura de chocolate"
     };
 
     static float[] precos = 
     {
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
+        6,48,
+        15,98,
+        9,90,
+        8,50,
+        12,
+        10,20,
+        5
     };
 }
